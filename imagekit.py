ae2ebee7a3a1b64f3ccb6087328d3542cae69d72
“JLO64” <64julianlopez@gmail.com>
2023-11-17 23:07:15 -0800

    py script to upload imgs done, added txt on hover

--- imagekit.py
+++ imagekit.py
@@ -20,8 +20,12 @@ def overwrite_file_with_markdown(file_path, markdown_text):
     print(file_path + " has been updated")
 
 def get_image_paths_from_markdown_file(markdown_text):
-    pattern = r'\!\[.*?\]\((.*?)\)'
-    matches = re.findall(pattern, markdown_text)
+    pattern_one = r'\!\[.*?\]\((.*?)\)'
+    pattern_two = r'url="(.*?)"'
+    matches_one = re.findall(pattern_one, markdown_text)
+    matches_two = re.findall(pattern_two, markdown_text)
+    matches_one.extend(matches_two)
+    matches = [match for match in matches_one if 'images' in match and 'ik.imagekit.io' not in match]
     return matches
 
 def upload_image(filename, imagekit_foldername):
@@ -29,32 +33,33 @@ def upload_image(filename, imagekit_foldername):
         folder=imagekit_foldername
     )
     
-    print("Uploading " + filename)
+    print(" Uploading " + filename)
     upload = imagekit.upload(
-        file=open("images/" + filename, "rb"),
+        file=open("images/" + filename.replace("images/", ""), "rb"),
         file_name=filename,
         options=options
     )
-    print(filename + " was uploaded")
-    print(filename + "'s url is: '" + str(upload.url))
+    print(" " + filename + " was uploaded")
+    print(" " + filename + "'s url is: '" + str(upload.url))
 
     #print(upload.response_metadata.raw)
 
     return str(upload.url)
 
 post_folder = "_posts/"
-post_filename = "2023-11-16-setting-up-my-domain.md"
 
-markdown_text = import_markdown(post_folder + post_filename)
+for post_filename in os.listdir(post_folder):
+    markdown_text = import_markdown(post_folder + post_filename)
 
-blog_image_filepaths = get_image_paths_from_markdown_file(markdown_text)
-if len(blog_image_filepaths) > 0:
-    print(len(blog_image_filepaths) + " images to process found")
-    for image_filepath in blog_image_filepaths:
-        image_filename = image_filepath.replace("../images/", "")
-        image_url = upload_image(filename=image_filename, imagekit_foldername="/www_julianlopez_net/" + post_filename.replace(".md","") + "/")
-        markdown_text = markdown_text.replace(image_filepath, image_url)
-else:
-    print("No images to process found")
-
-overwrite_file_with_markdown(post_folder + post_filename, markdown_text)
+    blog_image_filepaths = get_image_paths_from_markdown_file(markdown_text)
+    if len(blog_image_filepaths) > 0:
+        print( " " + str(len(blog_image_filepaths)) + " images to process found")
+        for image_filepath in blog_image_filepaths:
+            image_filename = image_filepath.replace("../images/", "")
+            image_url = upload_image(filename=image_filename, imagekit_foldername="/www_julianlopez_net/" + post_filename.replace(".md","") + "/")
+            markdown_text = markdown_text.replace(image_filepath, image_url)
+            os.remove(image_filepath.replace("../", ""))
+            print(" " + image_filepath + " has been deleted")
+        overwrite_file_with_markdown(post_folder + post_filename, markdown_text)
+    else:
+        print(" No images to process found")
