b59985ada4128735b7a4b945c26a468b4d0dcb5a
“JLO64” <64julianlopez@gmail.com>
2023-11-21 17:49:28 -0800

    Add contents of _site folder to gh-pages branch

--- _site/push-to-gh_pages.sh
+++ _site/push-to-gh_pages.sh
@@ -10,4 +10,5 @@ python3 imagekit.py
 bundle exec jekyll build
 
 # Push the contents of the _site folder to the github-pages branch, overwriting everything in the branch
-git push origin `git subtree split --prefix _site master`:gh-pages --force
+git checkout gh-pages && git rm -r --cached --ignore-unmatch . && git add _site/ --force && git commit -m "Add contents of _site folder to gh-pages branch" && git push origin gh-pages
+
