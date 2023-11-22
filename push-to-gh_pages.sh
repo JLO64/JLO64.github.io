a4de8c72a18c1408a30a683c5cbe7a259b5bccd4
“JLO64” <64julianlopez@gmail.com>
2023-11-21 16:50:54 -0800

    deleted GH build command, updated script

--- push-to-gh_pages.sh
+++ push-to-gh_pages.sh
@@ -4,10 +4,10 @@
 cd "$(dirname "$0")"
 
 # Run the imagekit.py script
-python imagekit.py
+python3 imagekit.py
 
 # Build the Jekyll site
 bundle exec jekyll build
 
-# Push the contents of the _site folder to the github-pages branch
-git subtree push --prefix _site origin github-pages
+# Push the contents of the _site folder to the github-pages branch, overwriting everything in the branch
+git push origin `git subtree split --prefix _site master`:gh-pages --force
