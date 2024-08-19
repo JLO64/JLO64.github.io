81bbd7e9dcbea726297ae655e0c0c42fe47f3a48
Julian Lopez <64julianlopez@gmail.com>
2024-08-19 16:21:47 -0700

    added github action to check Mastodon RSS with Cron

---
+++ _scripts/check_mastodon_rss.rb
@@ -0,0 +1,18 @@
+require 'rss'
+require 'open-uri'
+require 'yaml'
+
+rss_url = 'https://fosstodon.org/@JLO64.rss'
+rss_content = URI.open(rss_url).read
+rss = RSS::Parser.parse(rss_content, false)
+first_item = rss.items.first
+most_recent_mastodon_post_link = first_item.link
+
+most_recent_mastodon_post = YAML.load_file('_data/most_recent_mastodon_post.yml', permitted_classes: [Time])
+stored_most_recent_mastodon_post_link = most_recent_mastodon_post['link']
+
+if most_recent_mastodon_post_link != stored_most_recent_mastodon_post_link
+    puts "New post detected: #{most_recent_mastodon_post_link}"
+else
+    puts "No new post detected"
+end
