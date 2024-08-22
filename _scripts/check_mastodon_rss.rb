8e69ca87d64273712f51d86051ea20d36a8a2ac1
Julian Lopez <64julianlopez@gmail.com>
2024-08-22 09:47:50 -0700

    updated actions trigger

--- _scripts/check_mastodon_rss.rb
+++ _scripts/check_mastodon_rss.rb
@@ -12,8 +12,8 @@ most_recent_mastodon_post = YAML.load_file('_data/most_recent_mastodon_post.yml'
 stored_most_recent_mastodon_post_link = most_recent_mastodon_post['link']
 
 if most_recent_mastodon_post_link != stored_most_recent_mastodon_post_link
-    puts "New post detected: #{most_recent_mastodon_post_link}"
+    puts "true"
 else
-    puts "No new post detected"
-    exit 1
+    puts "false"
+    # exit 1
 end
