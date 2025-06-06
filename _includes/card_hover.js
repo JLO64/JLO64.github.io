d21edbd8047ea3fe861f82c250ec43997496ea07
Julian Lopez <64julianlopez@gmail.com>
2025-06-05 22:08:09 -0700

    Added text and card hover for homepage books

--- _includes/card_hover.js
+++ _includes/card_hover.js
@@ -13,3 +13,4 @@ function addAnimation(elements, animation) {
   addAnimation(document.querySelectorAll('.project-box'), 'card_breathe 0.5s infinite alternate linear');
   addAnimation(document.querySelectorAll('.table-of-contents'), 'card_breathe 0.5s infinite alternate linear');
   addAnimation(document.querySelectorAll('.homepage-mastodon-post'), 'mastodon_card_breathe 0.5s infinite alternate linear');
+  addAnimation(document.querySelectorAll('.hardcover-book-item'), 'card_breathe 0.5s infinite alternate linear');
